25c0a143f004b40e7d83b1f7c2d6e54351c77428
김태영 <kimtaeyoung@0Taes-Mac.local>
2021-12-20 22:22:10 +0900

    AppLoading

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
+import AppLoading from 'expo-app-loading';
 import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import { Ionicons } from '@expo/vector-icons';
+import * as Font from 'expo-font';
+import { Asset } from 'expo-asset';
 import { StyleSheet, Text, View } from 'react-native';
-
 export default function App() {
+  const [loading, setLoading] = useState(true);
+  const onFinish = () => setLoading(false);
+  const preload = async () => {
+    const fontsToLoad = [Ionicons.font];
+    const fontPromises = fontsToLoad.map((font) => Font.loadAsync(font));
+    const imagesToLoad = [
+      require('./assets/logo.png'),
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/840px-Instagram_logo.svg.png',
+    ];
+    const imagePromises = imagesToLoad.map((image) => Asset.loadAsync(image));
+    Promise.all([...fontPromises, ...imagePromises]);
+  };
+
+  if (loading) {
+    return (
+      <AppLoading
+        startAsync={preload}
+        onError={console.warn}
+        onFinish={onFinish}
+      />
+    );
+  }
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style='auto' />
     </View>
   );
 }
